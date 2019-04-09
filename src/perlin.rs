902f49cd6f2549bcc1f80daa5ddee0164ec2b4e2
Francesco MDE <francesco.montecuccolideglierri@unicredit.eu>
2019-04-09 15:55:18 +0200

    chapter 4.3

--- src/perlin.rs
+++ src/perlin.rs
@@ -27,6 +27,20 @@ fn perlin_generate_perm() -> Vec<usize> {
     p
 }
 
+fn trilinear_interop(c: &[[[f32; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+    let mut accum = 0.0;
+    for i in 0..2 {
+        for j in 0..2 {
+            for k in 0..2 {
+                accum += (i as f32 * u + (1 - i) as f32 * (1.0 - u)) *
+                    (j as f32 * v + (1 - j) as f32 * (1.0 - v)) *
+                    (k as f32 * w + (1 - k) as f32 * (1.0 - w)) * c[i][j][k];
+            }
+        }
+    }
+    accum
+}
+
 #[derive(Clone)]
 pub struct Perlin {
     ran_float: Vec<f32>,
@@ -46,9 +60,21 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Vector3<f32>) -> f32 {
-        let i = (4.0 * p.x) as usize & 255;
-        let j = (4.0 * p.y) as usize & 255;
-        let k = (4.0 * p.z) as usize & 255;
-        self.ran_float[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        let u = p.x - f32::floor(p.x);
+        let v = p.y - f32::floor(p.y);
+        let w = p.z - f32::floor(p.z);
+        let i = f32::floor(p.x) as usize;
+        let j = f32::floor(p.y) as usize;
+        let k = f32::floor(p.z) as usize;
+        let mut c = [[[0.0; 2]; 2]; 2];
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] =
+                        self.ran_float[self.perm_x[(i + di) & 255] ^ self.perm_y[(j + dj) & 255] ^ self.perm_z[(k + dk) & 255]]
+                }
+            }
+        };
+        trilinear_interop(&c, u, v, w)
     }
 }
