a498629b9f4a2693ad70d8c008f5084a97ed2860
Francesco MDE <francesco.montecuccolideglierri@unicredit.eu>
2019-04-10 11:28:06 +0200

    chapter 4.6

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,11 +1,14 @@
 use nalgebra::Vector3;
 use rand::Rng;
 
-fn perlin_generate() -> Vec<f32> {
+fn perlin_generate() -> Vec<Vector3<f32>> {
     let mut rng = rand::thread_rng();
     let mut p = Vec::with_capacity(256);
     for _ in 0..256 {
-        p.push(rng.gen::<f32>());
+        p.push(Vector3::new(
+            -1.0 + 2.0 * rng.gen::<f32>(),
+            -1.0 + 2.0 * rng.gen::<f32>(),
+            -1.0 + 2.0 * rng.gen::<f32>()).normalize());
     }
     p
 }
@@ -27,23 +30,28 @@ fn perlin_generate_perm() -> Vec<usize> {
     p
 }
 
-fn trilinear_interp(c: &[[[f32; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+fn perlin_interp(c: &[[[Vector3<f32>; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+    let uu = u * u * (3.0 - 2.0 * u);
+    let vv = v * v * (3.0 - 2.0 * v);
+    let ww = w * w * (3.0 - 2.0 * w);
     let mut accum = 0.0;
     for i in 0..2 {
         for j in 0..2 {
             for k in 0..2 {
-                accum += (i as f32 * u + (1 - i) as f32 * (1.0 - u)) *
-                    (j as f32 * v + (1 - j) as f32 * (1.0 - v)) *
-                    (k as f32 * w + (1 - k) as f32 * (1.0 - w)) * c[i][j][k];
+                let weight = Vector3::new(u - i as f32, v - j as f32, w - k as f32);
+                accum += (i as f32 * uu + (1 - i) as f32 * (1.0 - uu)) *
+                    (j as f32 * vv + (1 - j) as f32 * (1.0 - vv)) *
+                    (k as f32 * ww + (1 - k) as f32 * (1.0 - ww)) *
+                    c[i][j][k].dot(&weight);
             }
         }
     }
-    accum
+    (accum + 1.0) * 0.5
 }
 
 #[derive(Clone)]
 pub struct Perlin {
-    ran_float: Vec<f32>,
+    ran_vec: Vec<Vector3<f32>>,
     perm_x: Vec<usize>,
     perm_y: Vec<usize>,
     perm_z: Vec<usize>
@@ -52,7 +60,7 @@ pub struct Perlin {
 impl Perlin {
     pub fn new() -> Self {
         Perlin {
-            ran_float: perlin_generate(),
+            ran_vec: perlin_generate(),
             perm_x: perlin_generate_perm(),
             perm_y: perlin_generate_perm(),
             perm_z: perlin_generate_perm()
@@ -63,21 +71,18 @@ impl Perlin {
         let u = p.x - f32::floor(p.x);
         let v = p.y - f32::floor(p.y);
         let w = p.z - f32::floor(p.z);
-        let u = u * u * (3.0 - 2.0 * u);
-        let v = v * v * (3.0 - 2.0 * v);
-        let w = w * w * (3.0 - 2.0 * w);
         let i = f32::floor(p.x) as usize;
         let j = f32::floor(p.y) as usize;
         let k = f32::floor(p.z) as usize;
-        let mut c = [[[0.0; 2]; 2]; 2];
+        let mut c = [[[Vector3::new(0.0, 0.0, 0.0); 2]; 2]; 2];
         for di in 0..2 {
             for dj in 0..2 {
                 for dk in 0..2 {
                     c[di][dj][dk] =
-                        self.ran_float[self.perm_x[(i + di) & 255] ^ self.perm_y[(j + dj) & 255] ^ self.perm_z[(k + dk) & 255]]
+                        self.ran_vec[self.perm_x[(i + di) & 255] ^ self.perm_y[(j + dj) & 255] ^ self.perm_z[(k + dk) & 255]]
                 }
             }
         };
-        trilinear_interp(&c, u, v, w)
+        perlin_interp(&c, u, v, w)
     }
 }
